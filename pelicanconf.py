331f19b779092bb6ff292096ebe4be1ab0a02c80
suno-opnasd <58900068+shoh8@users.noreply.github.com>
2020-01-18 04:12:11 +0000

    pelican quickstart

---
+++ pelicanconf.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'shoh8'
+SITENAME = 'shoh8 github portal'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Tokyo'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None 
+AUTHOR_FEED_ATOM = None 
+AUTHOR_FEED_RSS = None 
+# Blogroll 
+LINKS = (
+         ('wish to connect to everywhere.', '#'),)
+
+# Social widget
+SOCIAL = (('twitter', 'https://twitter.com/shoh8'),
+        ('github', 'https://github.com/shoh8'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
